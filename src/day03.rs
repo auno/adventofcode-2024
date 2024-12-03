5bc15db92cd9ed7e66be1fc920670ec42bd8a1ae
Mikael Auno <mikael@auno.se>
2024-12-03 09:25:56 +0100

    Finished 2024/03b

--- src/day03.rs
+++ src/day03.rs
@@ -23,6 +23,35 @@ fn part1(input: &str) -> Result<u32> {
         .sum()
 }
 
+
+#[aoc(day3, part2)]
+fn part2(input: &str) -> Result<u32> {
+    let ins_pattern = Regex::new(r"(do\(\)|don't\(\)|mul\(\d+,\d+\))").unwrap();
+    let mul_pattern = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let (_, sum) = ins_pattern
+        .captures_iter(input)
+        .fold((true, 0), |(mul_enabled, sum), c| {
+            match c.extract() {
+                (_, ["do()"]) => (true, sum),
+                (_, ["don't()"]) => (false, sum),
+                (_, [mul]) => {
+                    if !mul_enabled {
+                        return (mul_enabled, sum)
+                    }
+
+                    let mul = mul_pattern.captures(mul).unwrap();
+                    let (_, [a, b]) = mul.extract();
+                    let a = a.parse::<u32>().unwrap();
+                    let b = b.parse::<u32>().unwrap();
+                    (mul_enabled, sum + a * b)
+                },
+            }
+        });
+
+    Ok(sum)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,5 +66,16 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(184576302, part1(&parse(include_str!("../input/2024/day3.txt"))).unwrap());
+    }
+
+    #[test]
+    fn part2_example1() {
+        let example1 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(48, part2(&parse(example1)).unwrap());
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(118173507, part2(&parse(include_str!("../input/2024/day3.txt"))).unwrap());
     }
 }
