5d7ec8071ba3670b10209081d1b6c56d61ccdc75
Mikael Auno <mikael@auno.se>
2024-12-04 12:55:27 +0100

    Clean up day 04

--- src/day04.rs
+++ src/day04.rs
@@ -16,65 +16,81 @@ fn parse(input: &str) -> HashMap<(isize, isize), char> {
         .collect()
 }
 
+fn resolve_candidates(
+    position: (isize, isize),
+    valid_patterns: &[Vec<((isize, isize), char)>],
+) -> impl Iterator<Item = Vec<((isize, isize), char)>> + use<'_> {
+    valid_patterns
+        .iter()
+        .map(move |valid_pattern| {
+            valid_pattern
+                .iter()
+                .copied()
+                .map(|(offset, valid_char)| ((position.0 + offset.0, position.1 + offset.1), valid_char))
+                .collect_vec()
+        })
+}
+
+fn is_valid_candiate(input: &HashMap<(isize, isize), char>, candidate: &[((isize, isize), char)]) -> bool {
+    candidate
+        .iter()
+        .all(|(position, expected_char)| {
+            input
+                .get(position)
+                .map_or(false, |found_char| found_char == expected_char)
+        })
+}
+
+fn resolve_candidate_position(position: &(isize, isize), found_char: char, expected_char: char) -> Option<(isize, isize)> {
+    if found_char == expected_char {
+        return Some(*position);
+    }
+
+    None
+}
+
+fn count_valid_positions(
+    input: &HashMap<(isize, isize), char>,
+    valid_patterns: &[Vec<((isize, isize), char)>],
+    candidate_position_char: char,
+) -> usize {
+    input
+        .iter()
+        .filter_map(|(position, c)| resolve_candidate_position(position, *c, candidate_position_char))
+        .flat_map(|candidate_position| resolve_candidates(candidate_position, valid_patterns))
+        .filter(|candidate| is_valid_candiate(input, candidate))
+        .count()
+}
+
 #[aoc(day4, part1)]
 fn part1(input: &HashMap<(isize, isize), char>) -> usize {
     const XMAS: [char; 4] = ['X', 'M', 'A', 'S'];
-
-    let xs = input
-        .iter()
-        .filter(|(_, c)| **c == XMAS[0])
-        .map(|(pos, _)| *pos)
-        .collect_vec();
-
-    xs.iter()
-        .map(|pos| {
-            [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-                .into_iter()
-                .filter(|dir| {
-                    let found = (0..4)
-                        .map(|offset| input
-                        .get(&(pos.0 + dir.0 * offset, pos.1 + dir.1 * offset)))
-                        .filter(Option::is_some)
-                        .map(Option::unwrap);
-
-                    found.eq(XMAS.iter())
-                })
-                .count()
-        })
-        .sum()
+    let valid_possibilities =
+        [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+            .into_iter()
+            .map(|(i, j): (isize, isize)| {
+                XMAS.iter()
+                    .enumerate()
+                    .skip(1) /* No need to check X */
+                    .rev() /* Significantly faster due border positions and input characteristicts */
+                    .map(|(offset, c)| (offset as isize, *c))
+                    .map(|(offset, c)| ((i * offset, j * offset), c))
+                    .collect_vec()
+            })
+            .collect_vec();
+    count_valid_positions(input, &valid_possibilities, XMAS[0])
 }
 
 #[aoc(day4, part2)]
 fn part2(input: &HashMap<(isize, isize), char>) -> usize {
-    let a_positions = input
-        .iter()
-        .filter(|(_, c)| **c == 'A')
-        .map(|(pos, _)| *pos)
-        .collect_vec();
-
-    let valid_possibilities = [
-        [((-1, -1), 'M'), ((1, 1), 'S'), ((-1, 1), 'M'), ((1, -1), 'S'), ((0, 0), 'A')],
-        [((-1, -1), 'S'), ((1, 1), 'M'), ((-1, 1), 'M'), ((1, -1), 'S'), ((0, 0), 'A')],
-        [((-1, -1), 'M'), ((1, 1), 'S'), ((-1, 1), 'S'), ((1, -1), 'M'), ((0, 0), 'A')],
-        [((-1, -1), 'S'), ((1, 1), 'M'), ((-1, 1), 'S'), ((1, -1), 'M'), ((0, 0), 'A')],
+    let valid_patterns = [
+        vec![((-1, -1), 'M'), ((1, 1), 'S'), ((-1, 1), 'M'), ((1, -1), 'S')],
+        vec![((-1, -1), 'S'), ((1, 1), 'M'), ((-1, 1), 'M'), ((1, -1), 'S')],
+        vec![((-1, -1), 'M'), ((1, 1), 'S'), ((-1, 1), 'S'), ((1, -1), 'M')],
+        vec![((-1, -1), 'S'), ((1, 1), 'M'), ((-1, 1), 'S'), ((1, -1), 'M')],
     ];
 
-    a_positions
-        .into_iter()
-        .filter(|pos| {
-            valid_possibilities
-                .into_iter()
-                .any(|valid_possibility| {
-                    valid_possibility
-                        .iter()
-                        .all(|(offset, valid_char)| {
-                            input
-                                .get(&(pos.0 + offset.0, pos.1 + offset.1))
-                                .map_or(false, |found_char| found_char == valid_char)
-                        })
-                })
-        })
-        .count()
+    count_valid_positions(input, &valid_patterns, 'A')
 }
 
 #[cfg(test)]
