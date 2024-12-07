88c5424111a458c6b62d5492c18f534689b333aa
Mikael Auno <mikael@auno.se>
2024-12-07 13:33:12 +0100

    Finished 2024/07a

---
+++ src/day07.rs
@@ -0,0 +1,92 @@
+use std::iter::repeat_n;
+
+use anyhow::{Context, Result};
+use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
+
+type Input = Vec<(u64, Vec<u64>)>;
+
+#[aoc_generator(day7)]
+fn parse(input: &str) -> Result<Input> {
+    input
+        .lines()
+        .map(|line| {
+            let mut numbers = line
+                .split([' ', ':'])
+                .filter(|s| !s.is_empty())
+                .map(|s| s.parse::<u64>().context(format!("Could not parse number: {s}")));
+
+            let test_value = numbers
+                .next()
+                .context("No test value found")??;
+
+            let operands = numbers.collect::<Result<_>>()?;
+
+            Ok((test_value, operands))
+        })
+        .collect()
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+enum Operator {
+    Add,
+    Multiply,
+}
+
+#[aoc(day7, part1)]
+fn part1(input: &Input) -> u64 {
+    input
+        .iter()
+        .filter_map(|(test_value, operands)| {
+            let first_operand = *operands.first()?;
+            repeat_n([Operator::Add, Operator::Multiply], operands.len() - 1)
+                .multi_cartesian_product()
+                .find_map(|operators| {
+                    let result = &operands[1..]
+                        .iter()
+                        .zip(operators.iter())
+                        .fold(first_operand, |result, (operand, operator)| {
+                            match operator {
+                                Operator::Add => result + operand,
+                                Operator::Multiply => result * operand,
+                            }
+                        });
+
+                    if result == test_value {
+                        return Some(test_value);
+                    }
+
+                    None
+                })
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    use super::*;
+
+    const EXAMPLE1: &str = indoc! {"
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+    "};
+
+    #[test]
+    fn part1_example1() {
+        assert_eq!(3749, part1(&parse(EXAMPLE1).unwrap()));
+    }
+
+    #[test]
+    fn part1_input() {
+        assert_eq!(1298300076754, part1(&parse(include_str!("../input/2024/day7.txt")).unwrap()));
+    }
+}
