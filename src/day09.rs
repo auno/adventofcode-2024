7666b0b14c8edaedd4f3be5f0d8c55ebce749b88
Mikael Auno <mikael@auno.se>
2024-12-09 13:19:14 +0100

    Finished 2024/09b

--- src/day09.rs
+++ src/day09.rs
@@ -4,35 +4,68 @@ use anyhow::{Context, Result};
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum DiskObject {
+    File(usize, usize),
+    Free(usize),
+}
+
+impl DiskObject {
+    fn len(&self) -> usize {
+        match *self {
+            DiskObject::File(len, _) => len,
+            DiskObject::Free(len) => len,
+        }
+    }
+}
+
 #[aoc_generator(day9)]
-fn parse(input: &str) -> Result<Vec<Option<usize>>> {
+fn parse(input: &str) -> Result<Vec<DiskObject>> {
     input
         .trim()
         .chars()
         .enumerate()
-        .map(|(i, c)| c.to_digit(10).map(|d| (i, d as usize)).context(format!("Unable to parse block size: {c}")))
-        .map_ok(|(i, len)| {
-            let element = if i % 2 == 0 { Some(i / 2) } else { None };
-            repeat_n(element, len)
+        .map(|(i, c)| {
+            c.to_digit(10)
+                .map(|d| (i, d as usize))
+                .context(format!("Unable to parse block size: {c}"))
         })
-        .flatten_ok()
+        .map_ok(|(i, len)| {
+            if i % 2 == 0 {
+                DiskObject::File(len, i / 2)
+            } else {
+                DiskObject::Free(len)
+            }
+        })
+        .collect()
+}
+
+fn generate_disk_map(input: &[DiskObject]) -> Vec<Option<usize>> {
+    input
+        .iter()
+        .flat_map(|disk_object| {
+            match *disk_object {
+                DiskObject::File(len, id) => repeat_n(Some(id), len),
+                DiskObject::Free(len) => repeat_n(None, len),
+            }
+        })
         .collect()
 }
 
 #[aoc(day9, part1)]
-fn part1(disk_map: &[Option<usize>]) -> usize {
-    let mut compacted = disk_map.to_vec();
+fn part1(input: &[DiskObject]) -> usize {
+    let mut disk_map = generate_disk_map(input);
 
     let mut a = 0;
-    let mut b = compacted.len() - 1;
+    let mut b = disk_map.len() - 1;
 
     while b > a {
-        match (compacted[a], compacted[b]) {
+        match (disk_map[a], disk_map[b]) {
             (Some(_), _) => {
                 a += 1;
             },
             (None, Some(_)) => {
-                compacted.swap(a, b);
+                disk_map.swap(a, b);
                 a += 1;
                 b -= 1;
             },
@@ -42,7 +75,54 @@ fn part1(disk_map: &[Option<usize>]) -> usize {
         }
     }
 
-    compacted
+    disk_map
+        .iter()
+        .enumerate()
+        .filter_map(|(i, b)| b.map(|f| i * f))
+        .sum()
+}
+
+#[aoc(day9, part2)]
+fn part2(input: &[DiskObject]) -> usize {
+    let mut disk = input.to_vec();
+    let file_ids = disk
+        .iter()
+        .filter_map(|disk_object| {
+            match *disk_object {
+                DiskObject::File(_, id) => Some(id),
+                DiskObject::Free(_) => None,
+            }
+        })
+        .collect_vec();
+
+    for id in file_ids.iter().rev() {
+        let Some((file_pos, &file)) = disk.iter().find_position(|disk_object| {
+            match *disk_object {
+                DiskObject::File(_, candidate_id) => candidate_id == id,
+                DiskObject::Free(_) => false,
+            }
+        }) else {
+            panic!("Unable to find file: {id}");
+        };
+
+        let Some((free_pos, &free)) = disk.iter().take(file_pos).find_position(|disk_object| {
+            match *disk_object {
+                DiskObject::File(_, _) => false,
+                DiskObject::Free(candidate_len) => *candidate_len >= file.len(),
+            }
+        }) else {
+            continue;
+        };
+
+        disk[file_pos] = DiskObject::Free(file.len());
+        disk[free_pos] = file;
+
+        if free.len() > file.len() {
+            disk.insert(free_pos + 1, DiskObject::Free(free.len() - file.len()));
+        }
+    }
+
+    generate_disk_map(&disk)
         .iter()
         .enumerate()
         .filter_map(|(i, b)| b.map(|f| i * f))
@@ -56,15 +136,15 @@ mod tests {
     const EXAMPLE1: &str = "2333133121414131402";
 
     #[test]
-    fn parse_example1() {
-        let parsed = parse(EXAMPLE1).unwrap();
-        let string_rep = parsed
+    fn generate_disk_map_example1() {
+        let disk_map = generate_disk_map(&parse(EXAMPLE1).unwrap());
+        let string_rep = disk_map
             .iter()
             .map(|b| match b {
                 Some(f) => f.to_string(),
                 None => ".".to_string(),
             })
-            .join("");
+            .collect::<String>();
         assert_eq!("00...111...2...333.44.5555.6666.777.888899", string_rep);
     }
 
@@ -76,5 +156,16 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(6378826667552, part1(&parse(include_str!("../input/2024/day9.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(2858, part2(&parse(EXAMPLE1).unwrap()));
+    }
+
+    #[test]
+    #[ignore]
+    fn part2_input() {
+        assert_eq!(6413328569890, part2(&parse(include_str!("../input/2024/day9.txt")).unwrap()));
     }
 }
