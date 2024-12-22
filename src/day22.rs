8338a57fa25cb6283b589f2125ccb20d71ec1691
Mikael Auno <mikael@auno.se>
2024-12-22 17:03:19 +0100

    Clean up day 22

--- src/day22.rs
+++ src/day22.rs
@@ -4,7 +4,7 @@ use anyhow::{Context, Result};
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 
-type Input = Vec<i64>;
+type Input = Vec<u64>;
 
 #[aoc_generator(day22)]
 fn parse(input: &str) -> Result<Input> {
@@ -14,35 +14,38 @@ fn parse(input: &str) -> Result<Input> {
         .collect()
 }
 
-fn secrets(initial_secret: i64) -> impl Iterator<Item = i64> {
+fn secrets(initial_secret: u64) -> impl Iterator<Item = u64> {
     iter::successors(Some(initial_secret), |previous| {
         let mut secret = *previous;
 
-        secret = ((secret << 6) ^ secret) % 16777216;
-        secret = ((secret >> 5) ^ secret) % 16777216;
-        secret = ((secret << 11) ^ secret) % 16777216;
+        secret = ((secret << 6) ^ secret) & 0xffffff;
+        secret = ((secret >> 5) ^ secret) & 0xffffff;
+        secret = ((secret << 11) ^ secret) & 0xffffff;
 
         Some(secret)
     })
 }
 
-fn prices(initial_secret: i64) -> impl Iterator<Item = i64> {
+fn prices(initial_secret: u64) -> impl Iterator<Item = usize> {
     secrets(initial_secret)
-        .map(|secret| secret % 10)
+        .map(|secret| (secret % 10) as usize)
 }
 
-fn pattern_index(a: i64, b: i64, c: i64, d: i64, e: i64) -> usize {
-    [b - a, c - b, d - c, e - d]
+fn changes(initial_secret: u64) -> impl Iterator<Item = (i64, usize)> {
+    prices(initial_secret)
+        .tuple_windows()
+        .map(|(a, b)| (b as i64 - a as i64, b))
+}
+
+fn pattern_index(a: i64, b: i64, c: i64, d: i64) -> usize {
+    [a, b, c, d]
         .into_iter()
         .map(|change| (change + 9) as usize)
-        .fold(0, |acc, v| {
-            assert!(v < (19));
-            (acc * 19) + v
-        })
+        .fold(0, |acc, v| (acc * 19) + v)
 }
 
 #[aoc(day22, part1)]
-fn part1(initial_secrets: &Input) -> i64 {
+fn part1(initial_secrets: &Input) -> u64 {
     initial_secrets
         .iter()
         .filter_map(|initial| secrets(*initial).nth(2000))
@@ -51,20 +54,20 @@ fn part1(initial_secrets: &Input) -> i64 {
 
 #[aoc(day22, part2)]
 fn part2(initial_secrets: &Input) -> usize {
-    let mut bananas = vec![0; 130321];
+    let mut total_bananas = vec![0; 130321];
 
     for &initial_secret in initial_secrets {
         let mut seen = vec![false; 130321];
 
-        for (a, b, c, d, e) in prices(initial_secret).take(2001).tuple_windows() {
-            let pattern = pattern_index(a, b, c, d, e);
+        for ((a, _), (b, _), (c, _), (d, bananas)) in changes(initial_secret).take(2000).tuple_windows() {
+            let pattern = pattern_index(a, b, c, d);
             if seen[pattern] { continue; }
-            bananas[pattern] += e as usize;
+            total_bananas[pattern] += bananas;
             seen[pattern] = true;
         }
     }
 
-    bananas.into_iter().max().unwrap()
+    total_bananas.into_iter().max().unwrap()
 }
 
 #[cfg(test)]
