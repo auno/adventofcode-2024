9bab453bd9fdabc59f8445483dcd9d914665bf6d
Mikael Auno <mikael@auno.se>
2024-12-23 15:40:14 +0100

    Finished 2024/23b

--- src/day23.rs
+++ src/day23.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 use anyhow::{Context, Result};
 use aoc_runner_derive::{aoc, aoc_generator};
@@ -43,6 +43,54 @@ fn part1(connections: &Input) -> usize {
         .into_iter()
         .filter(|group| group.iter().any(|computer| computer.starts_with("t")))
         .count()
+}
+
+#[aoc(day23, part2)]
+fn part2(connections: &Input) -> Option<String> {
+    let computers = connections.keys().map(String::as_str).sorted().collect_vec();
+    let computer_indices = computers.iter().enumerate().map(|(i, n)| (*n, i)).collect::<HashMap<_,_>>();
+    let connections = computers
+        .iter()
+        .map(|&computer| {
+            connections[computer]
+                .iter()
+                .map(String::as_str)
+                .map(|connection| computer_indices[connection])
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let mut best_clique = None;
+    let mut best_clique_len = 0;
+    let mut queue = (0..computers.len()).map(|i| (vec![i], connections[i].clone())).collect::<VecDeque<_>>();
+
+    while let Some((clique, candidates)) = queue.pop_front() {
+        if clique.len() + candidates.len() <= best_clique_len { continue; }
+        if candidates.is_empty() { continue; }
+
+        let candidate = candidates[0];
+
+        if clique.iter().all(|computer| connections[*computer].contains(&candidate)) {
+            let mut candidate_clique = clique.clone();
+            candidate_clique.push(candidate);
+            candidate_clique.sort();
+
+            if candidate_clique.len() > best_clique_len {
+                best_clique = Some(candidate_clique.clone());
+                best_clique_len = candidate_clique.len();
+            }
+
+            if candidates.len() > 1 {
+                queue.push_back((candidate_clique, candidates[1..].to_vec()));
+            }
+        }
+
+        if candidates.len() > 1 {
+            queue.push_back((clique, candidates[1..].to_vec()));
+        }
+    }
+
+    best_clique.map(|clique| clique.into_iter().map(|i| computers[i]).join(","))
 }
 
 #[cfg(test)]
@@ -94,5 +142,19 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(1314, part1(&parse(include_str!("../input/2024/day23.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_example1() {
+        let ans = part2(&parse(EXAMPLE1).unwrap());
+        assert_ne!(None, ans);
+        assert_eq!("co,de,ka,ta", ans.unwrap());
+    }
+
+    #[test]
+    fn part2_input() {
+        let ans = part2(&parse(include_str!("../input/2024/day23.txt")).unwrap());
+        assert_ne!(None, ans);
+        assert_eq!("bg,bu,ce,ga,hw,jw,nf,nt,ox,tj,uu,vk,wp", ans.unwrap());
     }
 }
